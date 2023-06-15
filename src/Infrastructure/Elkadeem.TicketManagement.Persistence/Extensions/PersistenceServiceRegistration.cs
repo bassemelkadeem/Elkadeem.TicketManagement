e1ad577d1567c23c89b062391b9b4c156658123a
Bassem Elsaeed <bassem.elkadeem@hotmail.com>
2023-06-15 20:17:36 +0300

    Modify in appsettings

--- src/Infrastructure/Elkadeem.TicketManagement.Persistence/Extensions/PersistenceServiceRegistration.cs
+++ src/Infrastructure/Elkadeem.TicketManagement.Persistence/Extensions/PersistenceServiceRegistration.cs
@@ -1,8 +1,6 @@
 ﻿using Elkadeem.TicketManagement.Application.Interfaces.Persistence;
 using Elkadeem.TicketManagement.Persistence.Repository;
 using Elkadeem.TicketManagement.Persistence.Shared;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Elkadeem.TicketManagement.Persistence.Extensions
@@ -11,24 +9,13 @@ namespace Elkadeem.TicketManagement.Persistence.Extensions
     {
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
         {
-            //services.AddDbContext<DatabaseContext>(options =>
-            //{
-            //    var connectionString = configuration.GetConnectionString("ElkadeemTicketConnectionString");
-            //    if (string.IsNullOrWhiteSpace(connectionString))
-            //    {
-            //        throw new NullReferenceException(nameof(connectionString) + " not found in appsettings.json");
-            //    }
-
-            //    options.UseSqlServer(connectionString);
-            //    options.EnableSensitiveDataLogging();
-            //});
-
             services.AddScoped<IDatabaseContext, DatabaseContext>();
             services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IEventRepository, EventRepository>();
             services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
