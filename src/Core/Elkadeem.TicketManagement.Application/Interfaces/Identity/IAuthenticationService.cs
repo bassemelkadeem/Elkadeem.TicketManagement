da2becb5792e1d31e01a7ce1d7bd62ed94dd8229
Bassem Elsaeed <bassem.elkadeem@hotmail.com>
2023-06-19 22:08:41 +0300

    Add UI

---
+++ src/Core/Elkadeem.TicketManagement.Application/Interfaces/Identity/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Elkadeem.TicketManagement.Application.Interfaces.Identity.Models;
+
+namespace Elkadeem.TicketManagement.Application.Interfaces.Identity
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+
+        Task<RegistrationResponse> RegisterAsync(RegistrationRequest request);
+    }
+}
